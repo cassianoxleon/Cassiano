e0c1d54a09fa6ba9b39be8661ee2491a2ba1f280
Your Name <you@example.com>
2026-05-19 19:56:52 -0300

    Lista de Compras Atualizada

--- exercicios/listadecompras.py
+++ exercicios/listadecompras.py
@@ -9,18 +9,25 @@ def verif_lista() -> bool:
 def remover_item(nome: str) -> str:
     if nome in lista_compras:
         lista_compras.remove(nome)
-        return "\n Item removido com sucesso!\n"
+        return f"\n Item removido com sucesso! ({nome})\n"    
     else:
         return "\n ⚠️ Item não encontrado.\n"
     
 def adicionar_item(nome: str) -> str:
     lista_compras.append(nome)
-    return "\n Item adicionado com sucesso!\n"
+    return f"\n Item adicionado com sucesso! ({nome})\n"
+
+def mostrar_lista():
+    if len(lista_compras) == 0:
+        print("👜 Lista de Compras: Não adicionou nenhum item.\n")
+    else:
+        print("👜 Lista de Compras: ", lista_compras, "\n")
     
 print("\n//------------------ 🛒 Shopee..? ------------------//")
 
 while True:
-    pergun = input("O que pretende fazer?\n(''ADD'' = adicionar; ''REMOVE'' = remover; ''END'' = terminar.) ").upper()
+    mostrar_lista()
+    pergun = input("O que pretende fazer?\n(''ADD'' = adicionar; ''REMOVE'' = remover; ''END'' = sair.) ").upper()
     if pergun == "REMOVE":
         verif = verif_lista()
         if verif:
@@ -37,17 +44,7 @@ while True:
     else:
         print("\n ⚠️ Por favor, escolha o que você pretende fazer.\n")
 
-    if len(lista_compras) == 0:
-        print("👜 Lista de Compras: Não adicionou nenhum item.\n")
-    else:
-        print("👜 Lista de Compras: ", lista_compras, "\n")
-
-if len(lista_compras) == 0:
-    print("👜 Lista de Compras: Não adicionou nenhum item.\n")
-else:
-    print("👜 Lista de Compras: ", lista_compras, "\n")
-print("Removendo todos os itens da sua Lista de Compras.")
-print("Removendo todos os itens da sua Lista de Compras..")
+mostrar_lista()
 print("Removendo todos os itens da sua Lista de Compras...")
-
+lista_compras.clear()
 print("\n 🎉 Parabéns, você acabou de desperdiçar seu tempo.")
